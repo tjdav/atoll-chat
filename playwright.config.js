786d515ebd450a7b561bab0d11f9e2b21eb56c73
Thomas David <tom@mightysmile.com>
2026-03-17 13:45:41 +0100

    chore: add server dev script and update Playwright webServer config

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'pnpm start',
+    command: 'pnpm test:server',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI
   }
