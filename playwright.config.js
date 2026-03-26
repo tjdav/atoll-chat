e46b0eee7091d40948a2f3712cc73cfecc9dffa9
Thomas David <tom@mightysmile.com>
2026-03-26 22:21:28 +0100

    chore(playwright): add tracker server to E2E test configuration

--- playwright.config.js
+++ playwright.config.js
@@ -35,9 +35,16 @@ export default defineConfig({
     }
   ],
 
-  webServer: {
-    command: 'pnpm test:server',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI
-  }
+  webServer: [
+    {
+      command: 'pnpm test:server',
+      url: 'http://localhost:3000',
+      reuseExistingServer: !process.env.CI
+    },
+    {
+      command: 'pnpm start:tracker',
+      url: 'http://localhost:8000/stats',
+      reuseExistingServer: !process.env.CI
+    }
+  ]
 })
