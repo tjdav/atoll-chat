c02b207a1c153441bc60aacc621c6282d72f5c94
Thomas David <tom@mightysmile.com>
2026-07-12 14:39:24 +0200

    chore: remove deprecated and dead code from messageUtils.js

--- src/utils/messageUtils.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * DEPRECATED: This utility is no longer used.
- * Message encryption and sending is now handled by the Web Worker.
- */
-export async function sendEncryptedMessage () {
-  throw new Error('sendEncryptedMessage is deprecated. Use the Web Worker SEND_MESSAGE task instead.')
-}
